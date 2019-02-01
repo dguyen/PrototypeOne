ee38b1cacde8220350443e09f7d8380e73da863c
bailey <bjin6@student.monash.edu>
2019-02-02 00:50:51 +1100

    Added bow and arrow placeholder models, weapon switching and bow shooting

---
+++ Assets/Scripts/Weapons/Bow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+
+    public Transform ArrowSpawn;
+    public Rigidbody Arrow;
+    public float ArrowSpeed;
+    private PlayerShowWeapon item;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        item = gameObject.GetComponent<PlayerShowWeapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if item1 (the bow) is active otherwise it wont shoot.
+        if(Input.GetButtonDown("Fire1") && item.item1.activeSelf == true)
+        {
+            Rigidbody arrowRigidbody;
+            arrowRigidbody = Instantiate(Arrow, ArrowSpawn.position, ArrowSpawn.rotation) as Rigidbody;
+            arrowRigidbody.AddForce(ArrowSpawn.forward * ArrowSpeed);
+        }
+    }
+}
