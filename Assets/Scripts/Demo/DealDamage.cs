02fd4d82787bf720c55506c408c6d4496eea443e
David Nguyen <davidngu28@gmail.com>
2019-02-01 17:08:16 +1100

    Added health to player, and also added a damagedealer for testing

---
+++ Assets/Scripts/Demo/DealDamage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour
+{
+    [Tooltip("How much damage to inflict")]
+    public int damageAmount = 20;
+
+    [Tooltip("The interval between damages")]
+    public float damageInterval = 1;
+
+    private Dictionary<Collider, IDamagable> activeEntities = new Dictionary<Collider, IDamagable>();
+
+    void OnTriggerEnter(Collider other) {
+        IDamagable[] damagableList = other.GetComponents<IDamagable>();
+        foreach (IDamagable damagable in damagableList) {
+            activeEntities.Add(other, damagable);
+            StartCoroutine(DamageLoop(other));
+        }
+    }
+    
+    void OnTriggerExit(Collider other) {
+        activeEntities.Remove(other);
+    }
+
+    IEnumerator DamageLoop(Collider other) {
+        IDamagable toDamage;
+        activeEntities.TryGetValue(other, out toDamage);
+        if (toDamage != null) {
+            yield return StartCoroutine(DoDamage(toDamage));
+            yield return StartCoroutine(DamageLoop(other));
+        }
+    }
+
+    IEnumerator DoDamage(IDamagable toDamage) {
+        toDamage.TakeDamage(damageAmount);
+        yield return new WaitForSeconds(damageInterval);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour, IDamagable
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    PlayerMovement playerMovement;
+    bool isDead;
+    bool damaged;
+
+    void Start() {
+        healthSlider.maxValue = startingHealth;
+        healthSlider.value = startingHealth;
+    }
+
+    void Awake () {
+        playerMovement = GetComponent <PlayerMovement> ();
+        currentHealth = startingHealth;
+    }
+
+    void Update () {
+        if(damaged) {
+            // Todo: Indicate damage
+        }
+        else {
+            // Todo: Clear damage indicator up
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount) {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        // Todo: Play damage sound
+        if(currentHealth <= 0 && !isDead) {
+            Death ();
+        }
+    }
+
+    void Death () {
+        isDead = true;
+        playerMovement.enabled = false;
+        // Todo: Play death sound
+    }
+}
