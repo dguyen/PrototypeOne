c61b1f34cbb37302e98b8e3ce6bac5ac0c2c144e
David Nguyen <davidngu28@gmail.com>
2019-02-02 01:35:21 +1100

    Added punching bag dummy to test weapons

---
+++ Assets/Scripts/Demo/FloatingText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    public float DestroyTime = 2f;
+    public Vector3 Offset = new Vector3(0, 2f, 0);
+    public Vector3 RngIntensity = new Vector3(0.25f, 0, 0);
+
+    void Start() {
+        Destroy(gameObject, DestroyTime);
+        transform.localPosition += Offset;
+        transform.localPosition += new Vector3(
+            Random.Range(-RngIntensity.x, +RngIntensity.x),
+            Random.Range(-RngIntensity.y, +RngIntensity.y),
+            Random.Range(-RngIntensity.z, +RngIntensity.z)  
+        );
+    }
+}
---
+++ Assets/Scripts/Demo/PunchingBag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PunchingBag : MonoBehaviour, IDamagable
+{
+    public GameObject FloatingTextPrefab;
+
+    public void TakeDamage(int damage) {
+        var tmp = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+        tmp.GetComponent<TextMesh>().text = damage.ToString();
+    }
+}
