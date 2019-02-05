5212976f0b43ef136fd5c9d0be2f594b64171e60
callmepho <47295382+callmepho@users.noreply.github.com>
2019-02-05 17:50:02 +1100

    Enemy Scripts
    Enemy Health
    Enemy Movement
    Enemy Attack

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, IDamagable
+{
+    public int spawnHealth = 100;
+    public int currentHealth;
+    BoxCollider boxCollider;
+    bool isDead;
+
+    void Awake()
+    {
+        currentHealth = spawnHealth;
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        if(isDead)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        currentHealth -= damage ;
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+
+        //Todo Damage particles
+    }
+
+    void Death()
+    {
+        isDead = true;
+        boxCollider.isTrigger = true;
+        //Todo Death animation
+    }
+}
