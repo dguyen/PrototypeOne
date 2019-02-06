2870cff8d5a4f66805dcf6d2b42dd060aec3c1e2
bailey <bjin6@student.monash.edu>
2019-02-06 20:41:01 +1100

    Added arrow damage

--- Assets/Scripts/Weapons/Arrow.cs
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public int perArrowDamage = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 5);
     }
 
-    void OnCollisionEnter()
+    private void OnCollisionEnter()
     {
         Destroy(gameObject);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamagable[] damagables = other.GetComponents<IDamagable>();
+        foreach (IDamagable damagable in damagables)
+        {
+            damagable.TakeDamage(perArrowDamage);
+        }
+        Destroy(gameObject);
+    }
+
 }
+
+
