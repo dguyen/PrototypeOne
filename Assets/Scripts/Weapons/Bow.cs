4060d02418c5f3c106d42be9e3fe5fc1aa8e4748
David Nguyen <davidngu28@gmail.com>
2019-02-10 01:17:51 +1100

    Added base class for weapon and rangedweapon, added ammo for ranged weapons, added UI for ammo count

--- Assets/Scripts/Weapons/Bow.cs
+++ Assets/Scripts/Weapons/Bow.cs
@@ -2,30 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bow : Entity
-{
+public class Bow : RangedWeapon {
     public Transform ArrowSpawn;
     public Rigidbody Arrow;
     public float ArrowSpeed;
 
-    private Inventory inventory;
-
-    void Start()
-    {
-        base.AddCapability(Capability.PICKABLE);
-        base.AddCapability(Capability.DROPABLE);
-        base.AddCapability(Capability.WEAPON);
-        inventory = FindObjectOfType<Inventory>();
-    }
-
-    void Update()
-    {
-        // Check if bow is being held otherwise it wont shoot.
-        if(inventory.GetSelectedItem() == gameObject && Input.GetButtonDown("Fire1"))
-        {
-            Rigidbody arrowRigidbody;
-            arrowRigidbody = Instantiate(Arrow, ArrowSpawn.position, ArrowSpawn.rotation) as Rigidbody;
+    public override void WeaponActive() {
+        if(Input.GetButtonDown("Fire1")) {
+            Rigidbody arrowRigidbody = Instantiate(Arrow, ArrowSpawn.position, ArrowSpawn.rotation) as Rigidbody;
             arrowRigidbody.AddForce(ArrowSpawn.forward * ArrowSpeed);
+            ammoCount--;
+            UpdateAmmoCount();
         }
     }
 }
---
+++ Assets/Scripts/Weapons/RangedWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RangedWeapon : Weapon {
+    public int ammoCount = 20;
+    public Text ammoCountText;
+
+    void OnEnable() {
+        UpdateAmmoCount();
+    }
+
+    void OnDisable() {
+        ammoCountText.text = "";
+    }
+
+    public override bool CanShoot() {
+        return WeaponSelected() && ammoCount > 0;
+    }
+
+    public void UpdateAmmoCount() {
+        if (ammoCountText) {
+            ammoCountText.text = ammoCount.ToString();
+        }
+    }
+}
