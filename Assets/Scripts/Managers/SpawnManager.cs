e265ed11d32ac19b0f0b6777cdc92e19d99fc78e
David Nguyen <davidngu28@gmail.com>
2019-02-09 15:34:55 +1100

    Added WaveManager and SpawnManager

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public int minimumSpawnAmount = 5;
+    public GameObject spawnArea;
+    public GameObject[] enemies;
+
+    private int enemiesLeft;
+    private int enemiesToSpawn;
+    private List<GameObject> spawnedEnemies;
+
+    public void SpawnNext(int wave) {
+        enemiesToSpawn = CalculateSpawnAmount(wave);
+        spawnedEnemies = new List<GameObject>();
+        StartCoroutine(BeginSpawning());
+    }
+
+    private int CalculateSpawnAmount(int wave) {
+        // Currently spawning at a (wave * 2 + minSpawn)
+        // Todo: Use different function i.e. logarithmic
+        return (wave * 2) + minimumSpawnAmount;
+    }
+
+    private IEnumerator BeginSpawning() {
+        yield return StartCoroutine(Spawn());
+        Debug.Log(enemiesToSpawn);
+        if (enemiesToSpawn <= 0) {
+            yield return null;
+        } else {
+            StartCoroutine(BeginSpawning());
+        }
+    }
+
+    private IEnumerator Spawn() {
+        Vector3 spawnLocation = GetRandomPosition();
+        spawnedEnemies.Add(Instantiate(enemies[0], spawnLocation, Quaternion.identity));
+        enemiesToSpawn--;
+        yield return new WaitForSeconds(1);
+    }
+
+    private Vector3 GetRandomPosition() {
+        float randomX = Random.Range(spawnArea.transform.position.x - (5 * spawnArea.transform.localScale.x / 2), spawnArea.transform.position.x + (5 * spawnArea.transform.localScale.x / 2));
+        float randomY = Random.Range(spawnArea.transform.position.y - (5 * spawnArea.transform.localScale.y / 2), spawnArea.transform.position.y + (5 * spawnArea.transform.localScale.y / 2));
+        float randomZ = Random.Range(spawnArea.transform.position.y - (5 * spawnArea.transform.localScale.z / 2), spawnArea.transform.position.y + (5 * spawnArea.transform.localScale.z / 2)); 
+        return new Vector3 (randomX, randomY, randomZ);
+    }
+
+    public int EnemiesLeft() {
+        spawnedEnemies.RemoveAll(item => item == null);
+        return spawnedEnemies.Count;
+    }
+}
---
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveManager : MonoBehaviour {
+    [Tooltip("The delay before the initial wave starts")]       
+    public float initialDelay = 5f;
+    
+    [Tooltip("The delay before the next wave starts")]       
+    public float waveDelay = 5f;
+
+    [Tooltip("The delay before the game over scene plays")]       
+    public float gameOverDelay = 1f;
+
+    [Tooltip("Text to indicate the current wave")]   
+    public Text waveText;
+
+    [Tooltip("Text to indicate the current wave")]   
+    public Text gameOverText;
+    
+    [Tooltip("Player health")]   
+    public PlayerHealth playerHealth;
+
+    [Tooltip("Player health")]   
+    public SpawnManager spawnManager;
+
+    private int currentWave = 0;
+    private WaitForSeconds initialStartDelay;
+    private WaitForSeconds waveEndWait;
+    private PlayerActionManager playerActionManager;
+
+    void Start() {
+        initialStartDelay = new WaitForSeconds(initialDelay);
+        waveEndWait = new WaitForSeconds(waveDelay);
+        StartCoroutine(GameLoop());
+    }
+
+    IEnumerator GameLoop() {
+        yield return StartCoroutine(WaveStarting());
+        yield return StartCoroutine(WavePlaying());
+        yield return StartCoroutine(WaveEnding());
+
+        if (playerHealth.currentHealth <= 0) {
+            GameOver();
+        } else {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    IEnumerator WaveStarting() {
+        currentWave++;
+        waveText.text = "Wave " + currentWave;
+        yield return null;
+    }
+
+    IEnumerator WavePlaying() {
+        spawnManager.SpawnNext(currentWave);
+
+        while (playerHealth.currentHealth > 0 && spawnManager.EnemiesLeft() > 0) {
+            yield return null;
+        }
+    }
+
+    IEnumerator WaveEnding() {
+        if (playerHealth.currentHealth <= 0) {
+            yield return new WaitForSeconds(gameOverDelay);
+        } else {
+            yield return waveEndWait;
+        }
+    }
+
+    void GameOver() {
+        waveText.text = "";
+        gameOverText.text = GameOverMessage();
+    }
+
+    string GameOverMessage() {
+        string message = "GAME OVER\n";
+        return message + "<color=#" + ColorUtility.ToHtmlStringRGB(new Color32(225, 123, 112, 225)) + "><size=20>Wave: " + currentWave + "</size></color>";
+    }
+}
