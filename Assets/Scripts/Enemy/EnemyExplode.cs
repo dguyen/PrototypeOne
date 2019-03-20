fe1ad4620c863c1199be2f06c2ed7b6dfddb892c
David Nguyen <davidngu28@gmail.com>
2019-03-20 14:47:58 +1100

    Added EnemyExplode script to EnemyToo

---
+++ Assets/Scripts/Enemy/EnemyExplode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyExplode : MonoBehaviour
+{
+    [Tooltip("The range that damage will be applied in")]
+    public float explosionRadius;
+
+    public LayerMask playerMask;
+    public ParticleSystem explosionParticles;
+    public int explosionDamage;
+
+    private EnemyHealth health;
+
+    void Awake() {
+        health = GetComponent<EnemyHealth>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player") || health.currentHealth <= 0)
+        {
+            return;
+        }
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, playerMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody)
+            {
+                continue;
+            }
+
+            IDamagable damagable = targetRigidbody.GetComponent<IDamagable>();
+            if (damagable == null)
+            {
+                continue;
+            }
+
+            damagable.TakeDamage(explosionDamage);
+        }
+
+        explosionParticles.transform.parent = null;
+        explosionParticles.Play();
+        Destroy(explosionParticles.gameObject, explosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+}
