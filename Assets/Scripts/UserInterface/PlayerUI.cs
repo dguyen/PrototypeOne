b34890795f7baee3005b00c9d6f32ef3ebee8c16
David Nguyen <davidngu28@gmail.com>
2019-04-03 22:04:57 +1100

    Added UI prefabs for co-op, added script to contain references to UI elements

---
+++ Assets/Scripts/UserInterface/PlayerUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI: MonoBehaviour {
+    public int PlayerNumber;
+    public Slider HealthSlider;
+    public Slider StaminaSlider;
+    public Slider ChargeSlider;
+    public Text MoneyText;
+    public Text AmmoCountText;
+    public InventoryUI InventoryUI;
+}
