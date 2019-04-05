2497266facfd5b54996b8e51c64489e1779b3960
David Nguyen <davidngu28@gmail.com>
2019-04-05 18:09:53 +1100

    Added GameMenuCamera and Manager to handle camera movement

---
+++ Assets/Scripts/Camera/GameMenuCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuCamera : MonoBehaviour {
+    public GameMenuManager gameMenuManager;
+
+    /**
+     * Show the character select UI
+     */
+    public void ShowCharSelectUI() {
+        gameMenuManager.ShowCharSelectUI();
+    }
+
+    /**
+     * Show the Main Menu UI (Start, settings, exit, etc)
+     */
+    public void ShowMainMenuUI() {
+        gameMenuManager.ShowMainMenuUI();
+    }
+}
---
+++ Assets/Scripts/Managers/GameMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour {
+    public Animator CameraAnimator;
+    public Animator CanvasAnimator;
+
+    /**
+     * Shift the camera view towards the main menu area (start, settings, exit, etc)
+     */
+    public void ShowGameMenu() {
+        CanvasAnimator.SetTrigger("HideCharPanel");
+        CameraAnimator.SetTrigger("GameMenuSelect");
+    }
+
+    /**
+     * Shift the camera view towards the character select area
+     */
+    public void ShowCharacterSelect() {
+        CameraAnimator.SetTrigger("CharacterSelect");
+        CanvasAnimator.SetTrigger("HideMainPanel");
+    }
+
+    /*
+     * ShowMainMenuUI will be called when camera animation finishes via animation event
+     */
+    public void ShowMainMenuUI() {
+        CanvasAnimator.SetTrigger("ShowMainPanel");
+    }
+
+    /*
+     * ShowCharSelectUI will be called when camera animation finishes via animation event
+     */
+    public void ShowCharSelectUI() {
+        CanvasAnimator.SetTrigger("ShowCharPanel");
+    }
+}
