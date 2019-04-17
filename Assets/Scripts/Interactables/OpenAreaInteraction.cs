cc44e7dfbd46b567a43c8960929f001135f3f3bd
David Nguyen <davidngu28@gmail.com>
2019-04-17 21:40:29 +1000

    Added open interaction for player

---
+++ Assets/Scripts/Interactables/OpenAreaInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAreaInteraction : MonoBehaviour, IInteractable {
+    public Area AreaToOpen;
+    public int CostToOpen;
+
+    public void Interact(GameObject Player) {
+        PlayerMoney PMoney = Player.GetComponent<PlayerMoney>();
+
+        if (PMoney.DecreaseMoney(CostToOpen)) {
+            AreaToOpen.Open();
+            RemoveBlockage();
+        } else {
+            // Todo: Inform player "Funds lacking"
+        }
+    }
+
+    void RemoveBlockage() {
+        gameObject.SetActive(false);
+    }
+}
