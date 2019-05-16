999b0a2188e0db9e2851ba9332f3419b911f4c16
David Nguyen <davidngu28@gmail.com>
2019-05-16 19:39:10 +1000

    Fixed TestLevel to support Player

---
+++ Assets/Scripts/Demo/PlayerInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInit : MonoBehaviour {
+    public GameObject Player;
+    public CameraFollow CameraFollow;
+    public PlayerDetails PlayerDetails;
+
+    void Awake() {
+        CameraFollow.SetupCamera();
+        PlayerDetails pDetails = Player.AddComponent<PlayerDetails>();
+        pDetails.PlayerControlScheme = PlayerDetails.PlayerControlScheme;
+        pDetails.PlayerNumber = PlayerDetails.PlayerNumber;
+        pDetails.PlayerUI = PlayerDetails.PlayerUI;
+    }
+}
