a96b2f4e083292cdb620bad3b9a2954949bc2922
David Nguyen <davidngu28@gmail.com>
2019-05-26 22:25:00 +1000

    Added EnemyRangedAttack script

---
+++ Assets/Scripts/Enemy/EnemyAttack/EnemyRangedAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangedAttack : EnemyAttack {
+    public GameObject Projectible;
+    public Transform AttackSpawn;
+
+    public override void Attack(GameObject player) {
+        GameObject Attack = Instantiate(Projectible, AttackSpawn.position, AttackSpawn.rotation);
+        Projectile EnemyProjectile = Attack.GetComponent<Projectile>();
+        EnemyProjectile.Target = player;
+        EnemyProjectile.Fire();
+    }
+}
